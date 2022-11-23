3ec5d7f6e008e06e9e1c42305f10ed39dede06a8
Neo <c.min.tseng@gmail.com>
2022-11-23 22:33:13 +0800

    modify to Android Studio project

---
+++ TigerGame/src/test/java/com/ccsu/test/ExampleInstrumentedTest.kt
@@ -0,0 +1,10 @@
+package com.ccsu.test
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
